685bab4e32b388bbed0cf52ffd90321b809c9e65
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-11-08 14:46:20 -0800

    Initial Commit

---
+++ moodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  moodTracker
+//
+//  Created by Barbarian Mogul on 11/8/18.
+//  Copyright © 2018 Makhmud Islamov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
